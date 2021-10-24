4ef06628935911e027fae9d55aaec79b5c4ca7f6
NikitaVisheratin <visheratinn@gmail.com>
2021-10-24 19:14:39 +0300

    22.10.2021_1.cpp

---
+++ 22.10.2021_1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+void FillArray(int* array, int& len) {
+	std::cout << "Input line:\n";
+	for (int i = 0; i < len; ++i) {
+		int value;
+		std::cin >> value;
+		array[i] = value;
+	}
+}
+
+int* CreateArray(int& len) {
+	int* A = new int[len];
+	FillArray(A, len);
+	return A;
+}
+
+void ArrayOutput(int* A, int& len) {
+	for (int* p = A; p < A + len; ++p) {
+		std::cout << "[" << *p << "] ";
+	}
+}
+
+void DeleteArray(int* A, int& len) {
+	delete[] A;
+}
+
+int main() {
+	int len;
+	std::cout << "Input array length:" << std::endl;
+	std::cin >> len;
+	int* array = CreateArray(len);
+	ArrayOutput(array, len);
+	DeleteArray(array, len);
+}
