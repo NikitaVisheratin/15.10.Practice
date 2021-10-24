f049e26b33045faf876b5b34474d8243390c2f88
NikitaVisheratin <visheratinn@gmail.com>
2021-10-24 19:39:15 +0300

    15.10.P.2

---
+++ Work_2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int main()
+{
+	int a;
+	char b;
+	bool c;
+	short d;
+	long e;
+	float f;
+	double g;
+	int h[] = { 1, 2, 3, 4, 5 };
+	std::cout << &a << " " << sizeof(a) << std::endl;
+	std::cout << &b << " " << sizeof(b) << std::endl;
+	std::cout << &c << " " << sizeof(c) << std::endl;
+	std::cout << &d << " " << sizeof(d) << std::endl;
+	std::cout << &e << " " << sizeof(e) << std::endl;
+	std::cout << &f << " " << sizeof(f) << std::endl;
+	std::cout << &g << " " << sizeof(g) << std::endl;
+	std::cout << &h << " " << sizeof(h) << std::endl;
+}
